83a3f5920be3614e46767475c1efcca9c0920360
hs985951 <39106682+hs985951@users.noreply.github.com>
2018-07-22 17:57:05 -0400

    main1
    main

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  C07380976_EXAM1_MAD3004
+//
+//  Created by yash on 21/07/18.
+//  Copyright © 2018 yash. All rights reserved.
+//
+
+import Foundation
+
+
+var o1 = order(orderid: 1, orderTotal: 1, ordercompanyname: "alpha", orderdelivercompanyname: "bestbuy" )
+o1.orderId = 1
+print(o1)
